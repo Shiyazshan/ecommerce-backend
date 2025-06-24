ecacead3819d18013d944883b50e26f177560c46
mohammed.shiyas <mohammed.shiyas@enpk.in>
2025-06-24 16:16:59 +0530

    create product api added

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,6 +8,22 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, price, description, image } = req.body;
+
+    if (!name || !price) {
+      return res.status(400).json({ error: "Name and price are required" });
+    }
+
+    const product = new Product({ name, price, description, image });
+    await product.save();
+
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 exports.getProductById = async (req, res) => {
   try {
